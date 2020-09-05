a2408f4dc8d42d0129b3a241d267b3c666cab244
BIGbratik <prayDEATH@mail.ru>
2020-09-05 12:02:54 +0300

    Доделал второе задание

--- src/Prakt1_6.java
+++ src/Prakt1_6.java
@@ -4,23 +4,6 @@ import java.util.Scanner;
 
 public class Prakt1_6 
 {
-	//Функция, выводящая остаток от деления параметров
-	public static double RemOfDiv (double Divined, double Divider)
-	{
-		double Rem;
-		Rem=Divined%Divider;
-		return Rem;
-	}
-	
-	//Функция, выводящая площадь треугольника (по высоте и основанию)
-	public static double AreaTriang (double footing, double height)
-	{
-		double Area;
-		Area=1/2*footing*height;
-		return Area;
-	}
-	
-	
 	public static void main(String[] args)
 	{
 		Scanner in = new Scanner(System.in);
@@ -31,21 +14,45 @@ public class Prakt1_6
 		{
 			//Практическое задание 1.1
 			case 1:
-				double Div_d, Div_r, Rem;
+				double div_d, div_r, rem;
 				System.out.println("\nВведите делимое и делитель (если вводите с плавающей точкой - вводите с запятой !!!)");
 				System.out.println("\nДелимое - ");
-				Div_d=in.nextDouble();
+				div_d=in.nextDouble();
 				System.out.println("\nДелитель - ");
-				Div_r=in.nextDouble();
-				Rem=RemOfDiv(Div_d,Div_r);
-				System.out.printf("\nОстаток от деления - %f",Rem);
+				div_r=in.nextDouble();
+				rem=remOfDiv(div_d,div_r);
+				System.out.printf("\nОстаток от деления - %f",rem);
 				break;
 		
 			//Практическое задание 1.2
 			case 2:
+				double foot, height, area;
+				System.out.println("\nВведите значения основания и высоты (если вводите с плавающей точкой - вводите с запятой !!!)");
+				System.out.println("\nОснование - ");
+				foot=in.nextDouble();
+				System.out.println("\nВысота - ");
+				height=in.nextDouble();
+				area=areaTriang(foot,height);
+				System.out.println("\nПлощадь треугольника - " + area);
+				break;
 			default:
 				System.out.println("Задания, номер которого вы ввели, не существует");
 		}
-		
+	}
+	
+	//Функция, выводящая остаток от деления параметров
+	public static double remOfDiv (double divined, double divider)
+	{
+		double rem;
+		rem=divined%divider;
+		return rem;
+	}
+	
+	//Функция, выводящая площадь треугольника (по высоте и основанию)
+	public static double areaTriang (double footing, double height)
+	{
+		double area;
+		area=(footing*height)/2;
+		return area;
 	}
 }
