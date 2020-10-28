84a2f18c475a580bcec6c75d596ad13e9c76b125
BIGbratik <prayDEATH@mail.ru>
2020-10-28 15:24:34 +0300

    Сделал 1 и 2 задания

--- src/Prakt3_6.java
+++ src/Prakt3_6.java
@@ -8,53 +8,95 @@ public class Prakt3_6
 		Scanner in = new Scanner(System.in);
 		
 		int choice;
-		System.out.println("\n Введите номер задания (от 1 до 10), которое хотите выполнить -");
+		System.out.print("\n Введите номер задания (от 1 до 10), которое хотите выполнить - ");
 		choice=in.nextInt();
+		in.nextLine();
 		
 		switch (choice)
 		{
-			//Практическое задание 1.1
+			//Практическое задание 3.1
 			case 1:
+				int a,b,c;
+				System.out.println("\n Введите значения A, B, C:");
+				a=in.nextInt();
+				b=in.nextInt();
+				c=in.nextInt();
+				in.nextLine();
+				
+				solutions(a,b,c);
 				break;
 		
-			//Практическое задание 1.2
+			//Практическое задание 3.2
 			case 2:
+				String strZip;
+				System.out.println("\n Введите строку:");
+				strZip=in.nextLine();
+				
+				findZip(strZip);
 				break;
 				
-			//Практическое задание 1.3
+			//Практическое задание 3.3
 			case 3:
 				break;
 				
-			//Практическое задание 1.4
+			//Практическое задание 3.4
 			case 4:
 				break;
 				
-			//Практическое задание 1.5
+			//Практическое задание 3.5
 			case 5:
 					break;
 			
-			//Практическое задание 1.6
+			//Практическое задание 3.6
 			case 6:
 				break;
 				
-			//Практическое задание 1.7
+			//Практическое задание 3.7
 			case 7:
 				break;
 				
-			//Практическое задание 1.8
+			//Практическое задание 3.8
 			case 8:
 				break;
 				
-			//Практическое задание 1.9
+			//Практическое задание 3.9
 			case 9:
 				break;
 				
-			//Практическое задание 1.10
+			//Практическое задание 3.10
 			case 10:
 				break;
 					
 			default:
 				System.out.println(" Задания, номер которого вы ввели, не существует");
 		}
+	}
+	
+	//Метод, определяющий сколько решений имеет квадратное уравнение, исходя из переданных коэффициентов
+	public static void solutions(int a,int b,int c)
+	{
+		double disc;
+		disc=Math.pow(b, 2) - 4 * a * c;
+		if (disc > 0)
+			System.out.println("Уравнение с данными коэффициентами имеет 2 решения");
+		else if (disc == 0)
+			System.out.println("Уравнение с данными коэффициентами имеет 1 решение");
+		else
+			System.out.println("Уравнение с данными коэффициентами не имеет  решений");
+	}
+	
+	//Метод, определяющий второе вхождение подстроки "zip" в строку
+	public static void findZip(String strZip)
+	{
+		int indexZip1, indexZip2;
+		indexZip1=strZip.indexOf("zip");
+		strZip=strZip.substring(indexZip1+3);
+		indexZip2=strZip.indexOf("zip");
+		if (indexZip2!=-1)
+				System.out.println("Индекс второго вхождения 'zip' в указанной строке: "
+						+(indexZip2+indexZip1+3+1));
+		else
+			System.out.println("Индекс второго вхождения 'zip' в указанной строке: "
+					+indexZip2);
 	}
 }
